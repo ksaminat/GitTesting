ec07df4b561012bf2ca97ffea0a7ce15ba89a271
kirubavinoth <kirubavinoth@192.168.2.3>
2015-08-09 00:15:00 +0530

    Git testing....

--- Testing/src/Test.java
+++ Testing/src/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Testing.. checkin.... Git....");
+		System.out.println("Testing.. checkin... Git...");
 	}
 
 }
